d517d0022bdaa9a7a79e76e2dc790c299cf59941
Antaca8 <antcardia4@alum.us.es>
2020-11-13 13:10:54 +0100

    Configuracion del repositorio

---
+++ Videojuegos/src/videojuegos_TEST.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 13 nov. 2020
+
+@author: anton
+'''
+from videojuegos import *
+
+if __name__ == '__main__':
+    VIDEOJUEGOS = lee_juegos('../data/videojuegos.csv')
+    
+    test_juegos()
+    
